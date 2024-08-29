50790ccfa767bb772eaf21343d5d1c240185d0c1
Matias0101 <maineromatias@gmail.com>
2024-08-28 21:20:36 -0300

    cambio de iconos / resourse / inc

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -3,7 +3,7 @@
 @includeWhen(class_exists(\Backpack\DevTools\DevToolsServiceProvider::class), 'backpack.devtools::buttons.sidebar_item')
 
 
-<x-backpack::menu-item title="Categories" icon="la la-question" :link="backpack_url('category')" />
-<x-backpack::menu-item title="Products" icon="la la-question" :link="backpack_url('product')" />
-<x-backpack::menu-item title="Users" icon="la la-question" :link="backpack_url('user')" />
-<x-backpack::menu-item title="Tags" icon="la la-question" :link="backpack_url('tag')" />
+<x-backpack::menu-item title="Categories" icon="la la-list" :link="backpack_url('category')" />
+<x-backpack::menu-item title="Products" icon="la la-box" :link="backpack_url('product')" />
+<x-backpack::menu-item title="Users" icon="la la-user" :link="backpack_url('user')" />
+<x-backpack::menu-item title="Tags" icon="la la-tag" :link="backpack_url('tag')" />
